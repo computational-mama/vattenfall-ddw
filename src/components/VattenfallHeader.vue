d6fce3a2e2cc19d061d626099ce83640731971fe
cmama <cmama@air.local>
2026-06-24 20:29:07 +0200

    added "built by WIP" in the header

--- src/components/VattenfallHeader.vue
+++ src/components/VattenfallHeader.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="bg-white kiosk:px-8 kiosk:py-6">
-    <header class="flex items-center justify-between">
+    <header class="">
       <!-- Vattenfall Logo -->
-      <div class="flex items-center">
+      <div class="flex items-center justify-between">
         <img
           src="/src/assets/logos/VF_logo_linear_grey_RGB.png"
           alt="Vattenfall"
           class="h-12 w-auto"
         />
+        <p class="text-xs text-gray-400 px-5">Built by Studio WIP</p>
+        <!-- <img src="/src/assets/logos/logowip.png" alt="Vattenfall" class="h-5 w-auto ml-3" /> -->
       </div>
     </header>
   </div>
